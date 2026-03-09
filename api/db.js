203e95d1d67e74b9780f213ced24a5f6fc7feb71
Pedroyamamotto <pedro.barreto@yamamotto.com>
2026-03-09 10:44:37 -0300

    feat: melhorar verificação de conexão com o MongoDB no cliente

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 let client;
 
 export async function getdb() {
-    if (!client) {
+    if (!client || client.topology.isDestroyed()) {
         client = new MongoClient(process.env.MONGODB_URI);
         await client.connect();
         console.log("Conectado ao MongoDB");
