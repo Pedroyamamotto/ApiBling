c58f026b854a59f6a7c01782d9f198bd260bf329
Pedroyamamotto <pedro.barreto@yamamotto.com>
2026-03-09 11:01:16 -0300

    fix: corrigir nome da função createTransporter para createTransport no serviço de email

--- api/utils/EmailServices.js
+++ api/utils/EmailServices.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 import "dotenv/config";
 
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // true for 465, false for other ports
